4e7c992f14aa50d88e9391e00fe6b1c265b37a1b
Dario Campagna <campagna@esteco.com>
2018-02-15 20:51:44 +0000

    Main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <algorithm>
+#include "fizzbuzz_factory.h"
 
 int main() {
-	std::cout << "Hello World!" << std::endl;
+	FizzBuzz* fizzBuzz = FizzBuzzFactory::game();
+	std::vector<int> numbers(100);
+	std::iota(numbers.begin(), numbers.end(), 1);
+	for(std::string numberAsText : fizzBuzz->print(numbers)) {
+		std::cout << numberAsText << " ";
+	}
 	return 0;
 }
